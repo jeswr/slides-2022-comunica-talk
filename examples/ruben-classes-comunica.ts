0c3d6b3c6f7602fc91225bb2a00c1299568c03d2
jeswr <63333554+jeswr@users.noreply.github.com>
2022-09-07 06:22:36 +0000

    Deploying to gh-pages from  @ ecc2d39161df66892f82eeaf8b0255afd0915616 🚀

---
+++ examples/ruben-classes-comunica.ts
@@ -0,0 +1,23 @@
+// import { QueryEngine } from '@comunica/query-sparql';
+import { QueryEngine } from '@comunica/query-sparql-reasoning';
+import { Bindings } from '@comunica/types';
+import context from './ruben-classes-context'
+
+async function main() {
+  const myEngine = new QueryEngine();
+  const bindingsStream = await myEngine.queryBindings(
+    'SELECT DISTINCT * WHERE { <https://www.rubensworks.net/#me> a ?o }',
+    context as any
+  );
+
+  // Consume results as a stream (best performance)
+  bindingsStream.on('data', (binding: Bindings) => {
+    // Obtaining values
+    console.log(binding.get('o')?.value);
+  });
+  bindingsStream.on('error', (error: Error) => {
+    console.error(error);
+  });
+}
+
+main();
---
+++ examples/ruben-classes-context.ts
@@ -0,0 +1,15 @@
+import { KeysRdfDereferenceConstantHylar, KeysRdfReason } from '@comunica/reasoning-context-entries';
+import { Store } from 'n3';
+
+export default {
+  sources: [
+    'https://www.rubensworks.net/',
+    // 'http://xmlns.com/foaf/spec/index.rdf'
+    // foaf is down (: so lets use this instead
+    'https://raw.githubusercontent.com/rdfjs/N3.js/6b9bb01258138472863efac80d9fff360e122bbe/perf/data/foaf.ttl',
+    // RDFS
+    // 'http://www.w3.org/2000/01/rdf-schema'
+  ],
+  [KeysRdfReason.rules.name]: KeysRdfDereferenceConstantHylar.rdfs,
+  [KeysRdfReason.implicitDatasetFactory.name]: () => new Store()
+};
